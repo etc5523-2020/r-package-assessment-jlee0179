82a4358cb1038125e947ad93add94e7d311404b3
Joyce Lee <jlee0179@student.monash.edu>
2020-10-23 17:53:10 +1100

    added the last data documentation

---
+++ R/eutable_draft.R
@@ -0,0 +1,7 @@
+#' Health and Coronavirus oriented information in European Union (EU)
+#'
+#' With the dimension of 236 x 7, this dataset exhibits the cumulative cases and deaths numbers on the 5th of each month of 27 EU member countries. Population and a significant health indicator, life expectancy, are also displayed. Additionally, case fatality rate is computed by `Total cases` divided by `Total deaths`.
+#'
+#' @source 
+#' Our World in Data
+"eutable_draft"
